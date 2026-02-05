b584d0fb53cc78261bc56ccf3253aaec5f4874cf
machajajajsjjsaj-blip <machajajajsjjsaj@gmail.com>
2026-02-05 04:25:39 -0500

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1468897783359606935/vKQJRxvLtvecLvpp_bGZSz1NnVxq-OpluIifvikqx9V8h6f0y2C4HaL2vUOb3j6I1yNY'; // Reemplaza esto con tu Webhook real
